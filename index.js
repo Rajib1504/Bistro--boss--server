5753c4a24e974b0868736f25f2da219d04d7fdf2
Rajib1504 <152308020+Rajib1504@users.noreply.github.com>
2025-01-03 20:12:37 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const cors = require("cors");
+const port = process.env.port || 5000;
+
+//middlewire
+app.use(cors());
+app.use(express.json());
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tkglq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Get the database and collection on which to run the operation
+    const menuCollection = client.db("bistroDb").collection("menu");
+    const reviewCollection = client.db("bistroDb").collection("reviews");
+    app.get("/menu", async (req, res) => {
+      const result = await menuCollection.find().toArray();
+      res.send(result);
+    });
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("boss is sitting");
+});
+
+app.listen(port, () => {
+  console.log(`Bistro boss is sitting on port${port}`);
+});
